aac79e3e450d352e7290a92a0af2d59c80bfcf06
Gus Vc <guzz.vc@gmail.com>
2025-04-01 19:00:39 -0600

    feat: preview photo added

--- src/Albums/components/AlbumItem.tsx
+++ src/Albums/components/AlbumItem.tsx
@@ -23,6 +23,8 @@ const AlbumItem = ({ album }: AlbumProps) => {
     const [uploadPhoto, resultUpload] = mutationUpload;
     const [savePhoto, resultSavePhoto] = mutationSavePhoto;
 
+    const [preview, setPreview] = useState<string | null>(null);
+
 
     const deleteAlbum = (e: React.FormEvent<HTMLFormElement>): void => {
         e.preventDefault();
@@ -56,6 +58,13 @@ const AlbumItem = ({ album }: AlbumProps) => {
         setIsUploadModalOpen(false);
     }    
 
+    const onChangePhotoHandler =() => {
+       const file = fileUploaderRef.current?.files?.[0];
+       if (file) {
+        setPreview(URL.createObjectURL(file));
+       }
+    }
+
     const header = <div className='flex flex-row items-start justify-between'>
         <p>{album.title}</p>
     </div>
@@ -80,7 +89,6 @@ const AlbumItem = ({ album }: AlbumProps) => {
 
             <h2 className="text-xl font-bold mb-4">Are you sure you want to delete this album?</h2>
             <form onSubmit={(e) => deleteAlbum(e)}>
-
                 <div className="flex flex-row space-x-2">
                     <button type="submit" className="bg-green-500 text-white p-2 w-full rounded mt-2"
                         disabled={resultSavePhoto.isLoading && resultUpload.isLoading}
@@ -102,17 +110,25 @@ const AlbumItem = ({ album }: AlbumProps) => {
                     type="file"
                     name="photo"
                     placeholder="Photo"
-                    onChange={() => { console.log('uploading photo') }}
+                    onChange={onChangePhotoHandler}
                     className="border p-2 w-full mb-2"
                     required
                     ref={fileUploaderRef}
                 />
 
+                {
+                    preview && <div className="flex flex-col items-center">
+                         <img src={preview} alt="Preview" className="mt-4 w-64 h-64 object-cover rounded-lg" />
+                    </div>
+                }
 
                 <button type="submit" className="bg-green-500 text-white p-2 w-full rounded mt-2">
                     Upload
                 </button>
-                <button onClick={() => setIsUploadModalOpen(false)} className="bg-red-500 text-white p-2 w-full rounded mt-2">
+                <button onClick={() => {
+                    setIsUploadModalOpen(false);
+                    setPreview(null);
+                }} className="bg-red-500 text-white p-2 w-full rounded mt-2">
                     Cancel
                 </button>
             </div>
--- src/components/UI/Modal.tsx
+++ src/components/UI/Modal.tsx
@@ -11,7 +11,7 @@ const Modal = ({ isOpen, onClose, children }: ModalProps) => {
   if (!isOpen) return null; // Do not render if not open
 
   return ReactDOM.createPortal(
-    <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50" onClick={onClose}>
+    <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 " onClick={onClose}>
       <div className="bg-white p-6 rounded-lg shadow-lg w-96">
         <button className="absolute top-2 right-2 text-gray-600" onClick={(e) => {
           e.stopPropagation();
